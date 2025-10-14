9763ca423cce3126ab5a97a995502587053c6331
AndyyBanh <andybanhab@gmail.com>
2025-10-14 16:53:52 -0400

    Project Seyup

---
+++ src/main/java/com/bathroombuddy/bathroombuddy/BathroombuddyApplication.java
@@ -0,0 +1,13 @@
+package com.bathroombuddy.bathroombuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BathroombuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BathroombuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bathroombuddy/bathroombuddy/BathroombuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bathroombuddy.bathroombuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BathroombuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
